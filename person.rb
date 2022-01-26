2b425ed528c9096cb5e7f0c9f38d3090025636fa
unknown <yisehakmitiku@gmail.com>
2022-01-26 16:20:26 +0800

    Fix linter errors

--- person.rb
+++ person.rb
@@ -1,8 +1,8 @@
 class Person
-  
-  def initialize(age, parent_permission: true, name = 'unknown')
+
+  def initialize(age, name = 'unknown', parent_permission: true,)
     @id = Random.rand(1..1000)
-    @name = age
+    @name = name
     @age = age
     @parent_permission = parent_permission
   end
--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require relative 'person'
 
 class Student < Person
   
-  def initialize(classroom, age, parent_permission: true, name = 'Unknown')
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
