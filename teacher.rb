a34650e7c149c4492fb989f1db659206b4a9022a
unknown <yisehakmitiku@gmail.com>
2022-01-27 04:35:08 +0800

    Update teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require relative 'person'
 
 class Teacher < Person
-  def initialize(age, specialization, name = 'Unknown')
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @specialization = specialization
   end
