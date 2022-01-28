f4d893eafd689f67e6ee79a213f9bf114f2c7b77
unknown <yisehakmitiku@gmail.com>
2022-01-28 16:15:04 +0800

    Create a method list rentals by person id

--- main.rb
+++ main.rb
@@ -155,4 +155,15 @@ def create_rental
   sleep 0.75
 end
 
+def list_rentals_by_person_id
+  print 'ID of person: '
+  id = gets.chomp.to_i
+
+  puts 'Rentals:'
+  @rentals.each do |rental|
+    puts "Date: #{rental.date}, Book '#{rental.book.title}' by #{rental.book.author}" if rental.person.id == id
+  end
+  sleep 0.75
+end
+
 end
