6e9da137df51d004755050925f528aec9de7cdd8
unknown <yisehakmitiku@gmail.com>
2022-01-28 15:30:39 +0800

    Setup association for student with classroom

--- classroom.rb
+++ classroom.rb
@@ -1,5 +1,15 @@
+require_relative './student'
+
 class Classroom
+  att_accessor :label
+  attr_reader :students
+
   def initialize(label)
      @label = label
+     @students = []
   end
+
+  def add_students(student)
+    @students.push(student)
+    studetn.classroom = self
 end
