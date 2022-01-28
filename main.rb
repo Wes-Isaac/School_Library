1d9dd6d15e3630ee49dfda2f8fdad8d3fa156e63
unknown <yisehakmitiku@gmail.com>
2022-01-28 16:11:23 +0800

    Create a method to create person

--- main.rb
+++ main.rb
@@ -69,4 +69,19 @@ class App
     sleep 0.75
   end
 
+  def create_person
+    print 'Do you want to create a student (1) or teacher (2) [Input a number]: '
+    option = gets.chomp
+
+    case option
+    when '1'
+      create_student
+    when '2'
+      create_teacher
+    else
+      puts 'Invalid input. Kindly type 1 or 2'
+    end
+  end
+
+
 end
