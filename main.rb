f714aa0b35ac4ef689dda34f19a5b3df890555c4
unknown <yisehakmitiku@gmail.com>
2022-01-28 16:08:47 +0800

    Create methods run and menu

--- main.rb
+++ main.rb
@@ -12,4 +12,29 @@ class App
     @rentals = []
     @class = Classroom.new('Grade 9')
   end
+
+  def run
+    puts 'Welcome to School Library!'
+    loop do
+      menu
+      option = gets.chomp
+      break if option == '7'
+
+      get_num option
+    end
+    puts 'Thank you for using our Library!'
+  end
+
+  def menu
+    puts
+    puts 'Please choose an option by entering a number'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
+  
 end
