3fdc470b28d69fec39bbe4c4f8f8cd442f6e9387
unknown <yisehakmitiku@gmail.com>
2022-01-28 15:34:26 +0800

    Setup association in rental

--- rental.rb
+++ rental.rb
@@ -1,7 +1,12 @@
 class Rental
   attr_accessor :date
+  attr_reader :book :person
 
-  def initialize(date)
+  def initialize(date, book, person)
     @date = date
+    @book = book
+    book.rental << self
+    @person = person
+    book.person << self
   end
 end
